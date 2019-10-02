6557d6d9401acc75f19fd4806d57bfac1caff405
Jerosik <yoon161@purdue.edu>
2019-10-02 17:40:37 -0400

    Read data by topics

--- src/actions/postActions.js
+++ src/actions/postActions.js
@@ -20,6 +20,25 @@ export const fetchPosts = () => dispatch => {
     });
 };
 
+export const fetchPostsByTopics = (query) => dispatch => {
+  db.collection('posts').where('topics', 'array-contains', query).orderBy('createdAt', 'desc').limit(10).get()
+    .then(function (snapshot) {
+      if (snapshot.empty) {
+        return;
+      }
+
+      let posts = [];
+      snapshot.forEach(doc => {
+        posts.push(doc.data());
+      })
+
+      dispatch({
+        type: FETCH_POSTS,
+        payload: posts
+      })
+    });
+};
+
 export const uploadPost = (text, topics) => dispatch => {
   let post = {
     text: text,
